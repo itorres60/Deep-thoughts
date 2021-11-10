99a6797340520ca9bd7da890785d840ca0bd1f60
J <torres.j.isaias@gmail.com>
2021-11-10 07:46:29 -0600

    added router

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,8 +7,8 @@ const secret = process.env.SECRET;
 const expiration = '2h';
 
 module.exports = {
-  signToken: function({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function({ user }) {
+    const payload = { user, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
